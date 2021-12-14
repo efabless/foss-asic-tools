3aa9b0ae306d16b3666f334d737b6d164471a5f9
kareefardi <kareemfarid@aucegypt.edu>
2021-12-15 00:33:50 +0200

    rename gaw3 image name in foss-asic-tools

--- images/opensta/scripts/dependencies.sh
+++ images/opensta/scripts/dependencies.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+set -x
 cmakeVersionBig=3.17
 cmakeVersionSmall=${cmakeVersionBig}.0
 
 install_cmake() {
-	wget https://cmake.org/files/v${cmakeVersionBig}/cmake-${cmakeVersionSmall}-Linux-x86_64.sh
+	wget https://cmake.org/files/v${cmakeVersionBig}/cmake-${cmakeVersionSmall}-Linux-x86_64.sh -q
 	chmod +x cmake-${cmakeVersionSmall}-Linux-x86_64.sh
 	./cmake-${cmakeVersionSmall}-Linux-x86_64.sh --skip-license --prefix=/usr/local
 }
 
 
 install_cmake
-
 
