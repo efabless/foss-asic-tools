4d36bce2b7782f978c2f9f097ba36e931e18d43a
Kareem Farid <karimmhany@gmail.com>
2022-01-25 14:51:01 +0200

    run.py better help messages

--- run.py
+++ run.py
@@ -6,6 +6,27 @@ from src.image.image import Image
 from src.docker_builder.docker_builder import DockerBuilder
 from src.image import paths as p
 
+
+class _HelpAction(argparse._HelpAction):
+    def __call__(self, parser, namespace, values, option_string=None):
+        parser.print_help()
+
+        # retrieve subparsers from parser
+        subparsers_actions = [
+            action for action in parser._actions
+            if isinstance(action, argparse._SubParsersAction)
+        ]
+        # there will probably only be one subparser_action,
+        # but better save than sorry
+        for subparsers_action in subparsers_actions:
+            # get all subparsers and print help
+            for choice, subparser in subparsers_action.choices.items():
+                print("Subparser '{}'".format(choice))
+                print(subparser.format_help())
+
+        parser.exit()
+
+
 def buildImages(paths):
     builder = DockerBuilder()
     images = []
@@ -15,6 +36,7 @@ def buildImages(paths):
         images.append(image)
 
     return images
+
 
 def build(args):
     all_flag = args.all
@@ -52,7 +74,7 @@ def update(args):
 def handleRecipe(args):
     json_file = args.json
     csv_file = args.csv
-    update = args.update
+    update = args.update_reference
     dictionary = None
 
     if json_file != None:
@@ -81,27 +103,39 @@ def handleRecipe(args):
 
 
 parser = argparse.ArgumentParser(
-    description="I don't know how to describe this yet"
-)
+    description="A tool that builds and updates a set of docker images",
+    add_help=False)
+
+parser.add_argument("-h",
+                    "--help",
+                    action=_HelpAction,
+                    help="show this help message and exit")
+
 subparsers = parser.add_subparsers()
+
 build_subparser = subparsers.add_parser("build",
-                                        help="build ")
+                                        help="build one or more images")
 build_subparser.add_argument("--all", action="store_true")
-build_subparser.add_argument("images", nargs='*', type=str)
+build_subparser.add_argument("images", nargs='+', type=str)
 build_subparser.set_defaults(func=build)
 
 update_subparser = subparsers.add_parser("update",
-                                         help="update")
+                                         help="update one or more image")
 update_subparser.add_argument("--all", action="store_true")
 update_subparser.add_argument("-y", "--yes", action="store_true")
-update_subparser.add_argument("images", nargs='*', type=str)
+update_subparser.add_argument("images", nargs='+', type=str)
 update_subparser.set_defaults(func=update)
 
 recipe_subparser = subparsers.add_parser("recipe",
-                                         help="recipe ")
-recipe_subparser.add_argument("--json", type=str)
-recipe_subparser.add_argument("--csv", type=str)
-recipe_subparser.add_argument("--update", action="store_true")
+                                         help="build images in a recipe file")
+recipe_group = recipe_subparser.add_mutually_exclusive_group(required=True)
+recipe_group.add_argument("--json", type=str)
+recipe_group.add_argument("--csv", type=str)
+recipe_subparser.add_argument(
+    "--update-reference",
+    help=
+    "update reference in a recipe file. Run without --update-reference to update the tool(s) afterwards",
+    action="store_true")
 recipe_subparser.set_defaults(func=handleRecipe)
 
 args = parser.parse_args()
