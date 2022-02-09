51d886f8b7c27b40732187bdf9955da21d088e6e
Mohamed Kassem <mkk@efabless.com>
2022-02-09 15:15:55 -0800

    some fixes

--- images/openroad/scripts/install.sh
+++ images/openroad/scripts/install.sh
@@ -17,8 +17,3 @@ make install
 
 
  
-sudo apt update
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
-
-curl -O https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tar.xz
-tar -xf Python-3.8.2.tar.xz
