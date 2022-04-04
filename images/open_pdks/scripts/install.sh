b7432f29d50a22cc4dd70d00303047ef82b82d5e
Harald Pretl <harald.pretl@jku.at>
2022-04-04 10:09:46 +0200

    Adapt to sky130A, save versions in SOURCES

--- images/open_pdks/scripts/install.sh
+++ images/open_pdks/scripts/install.sh
@@ -6,6 +6,7 @@ magic_version=$(ls /foss/tools/magic/ )
 export PATH=$PATH:/foss/tools/magic/$magic_version/bin/
 
 export PDK_ROOT=/foss/pdk
+export SKY130_VERSION=A
 
 cd $PDK_ROOT 
 git clone ${REPO_URL} ${NAME}
@@ -15,18 +16,20 @@ git pull
 git checkout -qf ${REPO_COMMIT}git clone https://github.com/RTimothyEdwards/open_pdks.git
 
 ./configure --enable-sky130-pdk=$PDK_ROOT/skywater-pdk  --enable-alpha-sky130 --enable-xschem-sky130 \
-            --enable-sram-sky130 --with-sky130-variants=all --datadir=/foss/
+            --enable-sram-sky130 --with-sky130-variants=$SKY130_VERSION --datadir=/foss/
 	    
+#make -j$(nproc)
+#make install
 make -j$(nproc)
+make install
 
-make install
-make -j$(nproc)
-make install
+echo "$NAME $REPO_COMMIT" 		> "$PDK_ROOT/sky130$SKY130_VERSION/SOURCES"
+echo "magic $magic_version" 		>> "$PDK_ROOT/sky130$SKY130_VERSION/SOURCES"
+cat "$PDK_ROOT/skywater-pdk/SOURCES" 	>> "$PDK_ROOT/sky130$SKY130_VERSION/SOURCES"
 
 make distclean
 
 cd $PDK_ROOT 
 rm -rf skywater-pdk open_pdks 
 chmod -R 755 $PDK_ROOT 
-
 
