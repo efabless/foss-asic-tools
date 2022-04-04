04fdd73c7223649aa5873101169c0fa559bcd96f
Harald Pretl <harald.pretl@jku.at>
2022-04-04 10:09:09 +0200

    Remove pull of extra libs to save space

--- images/skywater-pdk/scripts/install.sh
+++ images/skywater-pdk/scripts/install.sh
@@ -12,14 +12,16 @@ git submodule update --init libraries/sky130_fd_io/latest && \
 git submodule update --init libraries/sky130_fd_sc_hvl/latest && \
 git submodule update --init libraries/sky130_fd_pr/latest && \
 git submodule update --init libraries/sky130_fd_pr_reram/latest && \
-git submodule update --init libraries/sky130_fd_sc_lp/latest  && \
-git submodule update --init libraries/sky130_fd_sc_hs/latest  && \
-git submodule update --init libraries/sky130_fd_sc_ms/latest  && \
-git submodule update --init libraries/sky130_fd_sc_ls/latest  && \
-git submodule update --init libraries/sky130_fd_sc_hdll/latest && \
+#git submodule update --init libraries/sky130_fd_sc_lp/latest  && \
+#git submodule update --init libraries/sky130_fd_sc_hs/latest  && \
+#git submodule update --init libraries/sky130_fd_sc_ms/latest  && \
+#git submodule update --init libraries/sky130_fd_sc_ls/latest  && \
+#git submodule update --init libraries/sky130_fd_sc_hdll/latest && \
 git submodule update
 
 python3 -m pip install -e scripts/python-skywater-pdk
 mv $PDK_ROOT/make_timing.py .
 mv $PDK_ROOT/corners.yml .
 python3 ./make_timing.py
+
+echo "$NAME $REPO_COMMIT" > "$PDK_ROOT/$NAME/SOURCES"
