2fcdf2adb4dea64cc076c566eea7a235cdb44858
Harald Pretl <harald@pretl.eu>
2022-04-05 15:03:25 +0200

    Update xyce to Rocky8

--- images/xyce/scripts/dependencies.sh
+++ images/xyce/scripts/dependencies.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-yum -y install devtoolset-8-gcc-gfortran
-yum -y install make bison libtool
+yum -y install gcc-toolset-9-gcc-gfortran
+yum -y install make bison libtool cmake
 yum -y install suitesparse-devel flex-devel fftw-devel blas-devel lapack-devel openmpi-devel
 
-cmakeVersionBig=3.17
-cmakeVersionSmall=${cmakeVersionBig}.0
+#cmakeVersionBig=3.17
+#cmakeVersionSmall=${cmakeVersionBig}.0
 
-install_cmake() {
-	wget https://cmake.org/files/v${cmakeVersionBig}/cmake-${cmakeVersionSmall}-Linux-x86_64.sh
-	chmod +x cmake-${cmakeVersionSmall}-Linux-x86_64.sh
-	./cmake-${cmakeVersionSmall}-Linux-x86_64.sh --skip-license --prefix=/usr/local
-}
+#install_cmake() {
+#	wget https://cmake.org/files/v${cmakeVersionBig}/cmake-${cmakeVersionSmall}-Linux-x86_64.sh
+#	chmod +x cmake-${cmakeVersionSmall}-Linux-x86_64.sh
+#	./cmake-${cmakeVersionSmall}-Linux-x86_64.sh --skip-license --prefix=/usr/local
+#}
 
-
-install_cmake
+#install_cmake
