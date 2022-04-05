19ecf327cafd61935797db4238dc3a552eb07666
Harald Pretl <harald@pretl.eu>
2022-04-05 15:04:51 +0200

    Update gaw3-xschem to Rocky8

--- images/gaw3-xschem/scripts/install.sh
+++ images/gaw3-xschem/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source scl_source enable devtoolset-8
+source scl_source enable gcc-toolset-9
 
 git clone ${REPO_URL} ${NAME}
 cd ${NAME}
@@ -14,3 +14,4 @@ autoreconf -f -i
 ./configure --prefix=/foss/tools/${NAME}/${REPO_COMMIT}
 make -j$(nproc)
 make install
+
