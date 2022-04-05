e1fc30a60aa78de90c4f07e219334d8060a03e1d
Harald Pretl <harald@pretl.eu>
2022-04-05 22:02:28 +0200

    Update irsim to Rocky8

--- images/irsim/scripts/install.sh
+++ images/irsim/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source scl_source enable devtoolset-8
+source scl_source enable gcc-toolset-9
 
 git clone ${REPO_URL} ${NAME}
 cd ${NAME}
@@ -8,4 +8,3 @@ git checkout ${REPO_COMMIT}
 ./configure --prefix=/foss/tools/${NAME}/${REPO_COMMIT}
 make -j$(nproc)
 make install
-
