fd5259a33a17006a766f9bdcc5ed13d9aee91a0a
Harald Pretl <harald@pretl.eu>
2022-04-05 13:51:08 +0200

    Update xschem to Rocky8

--- images/xschem/scripts/dependencies.sh
+++ images/xschem/scripts/dependencies.sh
@@ -6,9 +6,7 @@ yum install -y \
 	libXpm-devel
 
 yum install -y libX11 libX11-devel libXrender libXrender-devel \
-		libxcb  libxcb-devel libcairo2 \
-		libcairo2-devel tcl tcl-devel tk tk-devel \
+		libxcb  libxcb-devel cairo \
+		cairo-devel tcl tcl-devel tk tk-devel \
 		flex bison libXpm libXpm-devel gawk 
-		
-		
 
