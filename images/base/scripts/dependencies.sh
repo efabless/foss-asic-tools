59a1220f274aa617befae6444d0d2d9920d97e99
Harald Pretl <harald@pretl.eu>
2022-04-05 13:49:57 +0200

    Update base to Rocky8

--- images/base/scripts/dependencies.sh
+++ images/base/scripts/dependencies.sh
@@ -4,22 +4,26 @@
 yum update -y
 #yum upgrade -y
 
-yum install -y https://repo.ius.io/ius-release-el7.rpm 
-yum install centos-release-scl -y
+#yum install -y https://repo.ius.io/ius-release-el7.rpm 
+yum install yum-utils -y
+dnf config-manager --set-enabled powertools
+yum install epel-release -y
+
+#yum install centos-release-scl -y
 yum install -y \
 	autoconf \
 	automake \
 	bison \
-	boost169-devel \
-	boost169-static \
+	boost-devel \
+	boost-static \
 	bzip2 \
 	cairo \
 	cairo-devel \
 	clang \
 	csh \
 	curl \
-	devtoolset-8 \
-	devtoolset-8-libatomic-devel \
+	gcc-toolset-9 \
+	gcc-toolset-9-libatomic-devel \
 	flex \
 	gawk \
 	gcc \
@@ -47,38 +51,40 @@ yum install -y \
 	ninja-build \
 	patch \
 	pcre-devel \
-	python-devel \
-	python36u \
-	python36u-devel \
-	python36u-libs \
-	python36u-pip \
-	python36u-tkinter \
+	python38 \
+	python38-devel \
+	python38-pip \
+	python38-tkinter \
+	python38-numpy \
+	python38-Cython \
+	python38-jinja2 \
+	python3-matplotlib \
+	python3-pandas \
+	python3-xlsxwriter \
 	readline-devel \
-	rh-python35 \
 	strace \
-	spdlog-devel \
-	swig3 \
+	swig \
 	tcl \
 	tcl-devel \
-	tcllib \
-	tclx \
 	texinfo \
 	tk \
 	tk-devel \
 	vim-common \
 	wget \
 	which \
-	xdot \
 	Xvfb \
 	zlib-devel \
 	zlib-static
+# 	xdot
 
-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 60
+#alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 60
 
-pip3.6 install --no-cache-dir --upgrade pip
-pip install --no-cache-dir \
-	matplotlib \
-	"jinja2<3.0.0" \
-	pandas \
-	install \
-	XlsxWriter
+#pip3.6 install --no-cache-dir --upgrade pip
+#pip install --no-cache-dir \
+#	matplotlib \
+#	"jinja2<3.0.0" \
+#	pandas \
+#	install \
+#	XlsxWriter
+
+pip3 install --no-cache-dir install
