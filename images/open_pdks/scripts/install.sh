5fa8144c76590364301458b27793207c9b4c715c
Harald Pretl <harald@pretl.eu>
2022-04-05 14:16:40 +0200

    Update open_pdks to Rocky8

--- images/open_pdks/scripts/install.sh
+++ images/open_pdks/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source scl_source enable devtoolset-8
+source scl_source enable gcc-toolset-9
 
 magic_version=$(ls /foss/tools/magic/ )
 export PATH=$PATH:/foss/tools/magic/$magic_version/bin/
