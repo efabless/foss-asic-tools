f78fefc4bcbdcdf1f6da879e55102ff8ef7499c4
Harald Pretl <harald@pretl.eu>
2022-04-06 10:56:32 +0200

    Build all for IIC

---
+++ build-iic.sh
@@ -0,0 +1,8 @@
+# Harald Pretl, IIC, JKU
+#
+# Build script for IIC docker images
+#
+# Order for building: base first, then magic; skywater-pdk before open_pdks; last is foss-asic-tools
+
+source venv/bin/activate
+python3 run.py build base magic xschem gaw3-xschem gtkwave netgen iic-osic irsim iverilog ngscope ngspice skywater-pdk open_pdks opensta xyce klayout foss-asic-tools
