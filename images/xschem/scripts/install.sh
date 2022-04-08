00d37e713f950d31f8b7147482e8ba8819decfc1
Harald Pretl <harald@pretl.eu>
2022-04-08 16:53:25 +0200

    Cleanup

--- images/xschem/scripts/install.sh
+++ images/xschem/scripts/install.sh
@@ -4,11 +4,9 @@ source scl_source enable gcc-toolset-9
 
 git clone ${REPO_URL} ${NAME}
 cd ${NAME}
-
-autoreconf -f -i
-
 git checkout ${REPO_COMMIT}
 
+autoreconf -f -i
 ./configure --prefix=/foss/tools/${NAME}/${REPO_COMMIT}
 make -j$(nproc)
 make install
