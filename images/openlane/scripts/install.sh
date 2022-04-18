ce667f581ef4274a81de787f8c16f5801085ff58
Harald Pretl <harald.pretl@jku.at>
2022-04-18 12:30:03 +0200

    OpenLane version not shortened

--- images/openlane/scripts/install.sh
+++ images/openlane/scripts/install.sh
@@ -3,8 +3,6 @@
 set -e
 source scl_source enable gcc-toolset-9
 
-REPO_COMMIT_SHORT=$(echo $REPO_COMMIT | cut -c 1-7)
-
 #install_openlane_tools() {
 #	cd docker
 #	make export-all
@@ -16,6 +14,6 @@ REPO_COMMIT_SHORT=$(echo $REPO_COMMIT | cut -c 1-7)
 #cd /foss/tools
 
 mkdir -p /foss/tools/
-git clone ${REPO_URL} /foss/tools/${NAME}/${REPO_COMMIT_SHORT}
-cd /foss/tools/${NAME}/${REPO_COMMIT_SHORT}
+git clone ${REPO_URL} /foss/tools/${NAME}/${REPO_COMMIT}
+cd /foss/tools/${NAME}/${REPO_COMMIT}
 git checkout ${REPO_COMMIT}
