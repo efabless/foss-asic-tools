6e330c59fa762d45f1de5ee42c2943430ae2f6f5
Harald Pretl <harald.pretl@jku.at>
2022-04-18 11:58:48 +0200

    Renamed build scripts

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,41 @@
-virtualenv -p python3 venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 run.py recipe --csv $1
+#!/bin/bash
+# Harald Pretl, IIC, JKU, 2022
+#
+# Build script for IIC docker images
+#
+# Order(s) for building: 
+#	first is `base`, then `magic`
+#	`skywater-pdk` before `open_pdks`
+#	`ghdl` and `yosys` must be built before `ghdl-yosys-plugin`
+#	last is `foss-asic-tools`
 
+if [ -z ${VIRTUAL_ENV+x} ]; then
+	source venv/bin/activate
+fi
+
+python3 run.py build \
+	base magic \
+	covered \
+	cvc-check \
+	fault \
+	gaw3-xschem \
+	gtkwave \
+	iic-osic \
+	irsim \
+	iverilog \
+	klayout \
+	netgen \
+	ngscope \
+	ngspice \
+	openlane \
+	openroad \
+	opensta \
+	padring \
+	qflow \
+	riscv-gnu-toolchain-rv32i \
+	skywater-pdk open_pdks \
+	verilator \
+	xschem \
+	xyce \
+	ghdl yosys ghdl-yosys-plugin \
+	foss-asic-tools
