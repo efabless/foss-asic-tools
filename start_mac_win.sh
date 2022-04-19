fe520ce3228e4ea5e31a2c372c9acd2d70ae76fc
Georg Zachl <it@zachlge.org>
2022-04-19 17:57:49 +0200

    Fix wrong X11 Port number (X11 counts 6000+display-number)

--- start_mac_win.sh
+++ start_mac_win.sh
@@ -3,11 +3,11 @@
 # Set the local X-Server to listen on TCP
 
 if [ -z ${DISP+z} ]; then
-	DISP="host.docker.internal:6000"
+	DISP="host.docker.internal:0"
 fi
 
 if [ -z ${DESIGNS+z} ]; then
-        DESIGNS="~/designs"
+        DESIGNS="./designs"
 fi
 
-docker run -d --user $(id -u):$(id -g) -e DISPLAY=$DISP -v $DESIGNS:/foss/designs:rw foss-asic-tools:alpha
+echo docker run -d --user $(id -u):$(id -g) -e DISPLAY=$DISP -v $DESIGNS:/foss/designs:rw foss-asic-tools:alpha
