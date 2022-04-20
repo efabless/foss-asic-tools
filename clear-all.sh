492408fdea12007cebdc2d1f03f64ef63d27ccf1
Georg Zachl <it@zachlge.org>
2022-04-20 11:13:16 +0200

    Of course, remove the echos in the script.

--- clear-all.sh
+++ clear-all.sh
@@ -7,9 +7,9 @@
 source venv/bin/activate
 
 # First, grep all containers that build on the foss-asic-tools container and delete them.
-docker ps -a |grep -i foss-asic-tools |cut -d " " -f 1 |xargs echo docker rm
+docker ps -a |grep -i foss-asic-tools |cut -d " " -f 1 |xargs docker rm
 
 # Then, for all images that are available for the build, try to find some and if they exist, delete all existing tags.
 for d in images/*/ ; do
-	docker images | grep -w "^`basename $d`" | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} echo docker rmi "`basename $d`:{}"
+	docker images | grep -w "^`basename $d`" | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi "`basename $d`:{}"
 done
