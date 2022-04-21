bf3699e7faa7bfcb9b8750f5324b5661e6c306c7
Harald Pretl <harald.pretl@jku.at>
2022-04-21 08:00:33 +0200

    Remove empty dependency build scripts

--- images/gaw3-xschem/scripts/dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#yum install -y \
-#	alsa-lib-devel \
-#	autoconf \
-#	automake \
-#	gtk3-devel \
-#	make
--- images/skywater-pdk/scripts/dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#python3 -m pip install  pyyaml
-#yum install -y python3-pyyaml
