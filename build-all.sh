dff1ccc771b2265eb77002643d6be237bd930557
Georg Zachl <it@zachlge.org>
2022-04-29 14:41:23 +0200

    Added variables to build-all.sh for Docker image name, tag and target platforms.

--- build-all.sh
+++ build-all.sh
@@ -7,6 +7,18 @@ if [ -z ${DOCKER_USER+z} ]; then
 	DOCKER_USER="hpretl"
 fi
 
+if [ -z ${DOCKER_IMAGE+z} ]; then
+        DOCKER_IMAGE="iic-osic-tools"
+fi
+
+if [ -z ${DOCKER_TAG+z} ]; then
+        DOCKER_TAG="latest"
+fi
+
+if [ -z ${DOCKER_PLATFORMS+z} ]; then
+	DOCKER_PLATFORMS="linux/amd64,linux/arm64/v8"
+fi
+
 set -e
 docker buildx create --name iic-osic-tools-builder --node iic-osic-tools-builder0 --bootstrap --config ./buildkitd.toml
-docker buildx build --platform linux/amd64,linux/arm64/v8 --builder iic-osic-tools-builder --push --tag ${DOCKER_USER}/iic-osic-tools:latest .
+docker buildx build --platform ${DOCKER_PLATFORMS} --builder iic-osic-tools-builder --push --tag ${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG} .
