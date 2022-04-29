0461a649dbb5092efe3c2b5664bf84767504e17d
Georg Zachl <it@zachlge.org>
2022-04-29 11:19:49 +0200

    Add variable with default for the docker user name in build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,10 @@
 #
 # Build script for IIC docker images
 
-set -e
+if [ -z ${DOCKER_USER+z} ]; then
+	DOCKER_USER="hpretl"
+fi
 
-docker buildx create --name iic-osic-tools-builder --node iic-osic-tools-builder0 --bootstrap --config ./buildkitd.toml
-docker buildx build --platform linux/amd64,linux/arm64/v8 --builder iic-osic-tools-builder --push --tag iic-osic-tools:latest .
+set -e
+echo docker buildx create --name iic-osic-tools-builder --node iic-osic-tools-builder0 --bootstrap --config ./buildkitd.toml
+echo docker buildx build --platform linux/amd64,linux/arm64/v8 --builder iic-osic-tools-builder --push --tag ${DOCKER_USER}/iic-osic-tools:latest .
