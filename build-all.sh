53a87dcdb29f9084a3a95d8eeead5afcee60f9ac
Georg Zachl <it@zachlge.org>
2022-05-05 16:36:54 +0200

    WIP: Multiarch build on two machines, currently only the future commands are printed!

--- build-all.sh
+++ build-all.sh
@@ -19,8 +19,26 @@ if [ -z ${DOCKER_PLATFORMS+z} ]; then
 	DOCKER_PLATFORMS="linux/amd64,linux/arm64/v8"
 fi
 
+if [ -z ${DOCKER_LOAD+z} ]; then
+	load_or_push="--push"
+else
+	load_or_push="--load"
+fi
+
 DATE_TAG=$(date +"%Y.%m")
 
 set -e
-docker buildx create --name iic-osic-tools-builder --node iic-osic-tools-builder0 --bootstrap --config ./buildkitd.toml
-docker buildx build --platform ${DOCKER_PLATFORMS} --builder iic-osic-tools-builder --push --tag ${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG} --tag ${DOCKER_USER}/${DOCKER_IMAGE}:${DATE_TAG} .
+
+BUILDER_NAME="iic-osic-tools-builder"
+
+#Todo for remote build!
+# remove qemu-user-static!
+# Create the local build context
+echo docker context create ${BUILDER_NAME}-amd64
+# create the remote build context
+echo docker context create ${BUILDER_NAME}-arm64 --docker host=ssh://ubuntu@193.122.149.200
+
+#docker buildx create --name iic-osic-tools-builder --node iic-osic-tools-builder0 --bootstrap --config ./buildkitd.toml
+echo docker buildx create --name ${BUILDER_NAME} --config ./buildkitd.toml ${BUILDER_NAME}-amd64
+echo docker buildx create --name ${BUILDER_NAME} --config ./buildkitd.toml --append ${BUILDER_NAME}-arm64
+echo docker buildx build --platform ${DOCKER_PLATFORMS} --builder ${BUILDER_NAME} ${load_or_push} --tag ${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG} --tag ${DOCKER_USER}/${DOCKER_IMAGE}:${DATE_TAG} .
