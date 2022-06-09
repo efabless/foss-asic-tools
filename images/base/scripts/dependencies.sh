dc0cb49284124e043e777e85a18e9ff2634416bf
Harald Pretl <harald@pretl.eu>
2022-06-09 18:30:34 +0200

    Add /usr/bin/time
    and fix a few ShellCheck warnings along the way

--- images/base/scripts/dependencies.sh
+++ images/base/scripts/dependencies.sh
@@ -125,6 +125,7 @@ yum install -y \
 	tcl \
 	tcl-devel \
 	texinfo \
+	time \
 	tk \
 	tk-devel \
 	unzip \
@@ -163,17 +164,18 @@ pip3 install --no-cache-dir \
 	volare>=0.1.3 \
 	spyci
 
-# eigen-3.3, lemon-1.3.1, boost-1.76.0 are required for OpenROAD (in OpenLane)
+# eigen-3.3, lemon-1.3.1, boost-1.76.0 are required for OpenROAD (which is used in OpenLane)
+# shellcheck disable=SC1091
 source scl_source enable gcc-toolset-9
 #
 # Install boost-1.76.0
 #
 install_boost () {
-	cd /tmp
+	cd /tmp || exit
 	wget --no-verbose https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz
 	md5sum -c <(echo "e425bf1f1d8c36a3cd464884e74f007a  boost_1_76_0.tar.gz") || exit 1
 	tar -xf boost_1_76_0.tar.gz
-	cd boost_1_76_0
+	cd boost_1_76_0 || exit
 	#FIXME somehow Python is not found by build script, thus need this WA
 	./bootstrap.sh --with-python=python3 --with-python-version=3.6 --with-python-root=/usr/bin/python3
 	sed -i 's+"/usr/bin/python3"+/usr/bin/python3 : /usr/include/python3.6m : /usr/lib+g' project-config.jam
@@ -184,24 +186,24 @@ install_boost
 # Install eigen-3.3
 #
 install_eigen () {
-	cd /tmp
+	cd /tmp || exit
 	git clone -b 3.3 https://gitlab.com/libeigen/eigen.git
-	cd eigen
+	cd eigen || exit
 	cmake -B build .
-	cmake --build build -j $(nproc) --target install
+	cmake --build build -j "$(nproc)" --target install
 }
 install_eigen
 #
 # Install lemon-1.3.1
 #
 install_lemon () {
-	cd /tmp
+	cd /tmp || exit
 	wget --no-verbose http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz
 	md5sum -c <(echo "e89f887559113b68657eca67cf3329b5  lemon-1.3.1.tar.gz") || exit 1
 	tar -xf lemon-1.3.1.tar.gz
-	cd lemon-1.3.1
+	cd lemon-1.3.1 || exit
 	cmake -B build .
-	cmake --build build -j $(nproc) --target install
+	cmake --build build -j "$(nproc)" --target install
 }
 install_lemon
 
