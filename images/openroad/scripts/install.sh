6797bb4a6301b115b2f880f2c08adb6f2d4a91a3
Georg Zachl <it@zachlge.org>
2022-06-23 08:50:20 +0200

    Further changes to the variable names to match openlanes internal names.

--- images/openroad/scripts/install.sh
+++ images/openroad/scripts/install.sh
@@ -3,13 +3,13 @@
 set -e
 source scl_source enable gcc-toolset-9
 
-REPO_COMMIT_SHORT=$(echo $OPENROAD_REPO_COMMIT | cut -c 1-7)
+REPO_COMMIT_SHORT=$(echo $OPENROAD_APP_REPO_COMMIT | cut -c 1-7)
 
-git clone --recursive ${OPENROAD_REPO_URL} ${OPENROAD_NAME}
-cd ${OPENROAD_NAME}
-git checkout ${OPENROAD_REPO_COMMIT}
+git clone --recursive ${OPENROAD_APP_REPO_URL} ${OPENROAD_APP_NAME}
+cd ${OPENROAD_APP_NAME}
+git checkout ${OPENROAD_APP_REPO_COMMIT}
 mkdir -p build/
 cd build
-CFLAGS='-Wno-narrowing' cmake .. "-DCMAKE_INSTALL_PREFIX=/foss/tools/${OPENROAD_NAME}/${REPO_COMMIT_SHORT}"
+CFLAGS='-Wno-narrowing' cmake .. "-DCMAKE_INSTALL_PREFIX=/foss/tools/${OPENROAD_APP_NAME}/${REPO_COMMIT_SHORT}"
 make -j$(nproc)
 make install
