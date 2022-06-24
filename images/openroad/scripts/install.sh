533820165eac488ac425f1b386d66c134e28eb3a
Georg Zachl <it@zachlge.org>
2022-06-24 15:48:27 +0200

    OpenROAD: Fixed build error (submodules are probably out of sync without submodule update after checkout). Deleted unused file.

--- images/openroad/scripts/install.sh
+++ images/openroad/scripts/install.sh
@@ -5,9 +5,11 @@ source scl_source enable gcc-toolset-9
 
 REPO_COMMIT_SHORT=$(echo $OPENROAD_APP_REPO_COMMIT | cut -c 1-7)
 
-git clone --recursive ${OPENROAD_APP_REPO_URL} ${OPENROAD_APP_NAME}
+git clone ${OPENROAD_APP_REPO_URL} ${OPENROAD_APP_NAME}
 cd ${OPENROAD_APP_NAME}
 git checkout ${OPENROAD_APP_REPO_COMMIT}
+git submodule update --init --recursive
+
 mkdir -p build/
 cd build
 CFLAGS='-Wno-narrowing' cmake .. "-DCMAKE_INSTALL_PREFIX=/foss/tools/${OPENROAD_APP_NAME}/${REPO_COMMIT_SHORT}"
