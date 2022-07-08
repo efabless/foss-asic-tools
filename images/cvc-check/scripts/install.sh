bc71282ca8b82a5619060d098294e53e52cfcbb9
Harald Pretl <harald@pretl.eu>
2022-07-08 17:20:16 +0200

    Update according doc

--- images/cvc-check/scripts/install.sh
+++ images/cvc-check/scripts/install.sh
@@ -10,7 +10,7 @@ REPO_COMMIT_SHORT=$(echo $CVC_REPO_COMMIT | cut -c 1-7)
 git clone ${CVC_REPO_URL} ${CVC_NAME}
 cd ${CVC_NAME}
 git checkout ${CVC_REPO_COMMIT}
-autoreconf -i
+autoreconf -vif
 ./configure --disable-nls --prefix=/foss/tools/${CVC_NAME}/${REPO_COMMIT_SHORT}
 make -j$(nproc)
 make install
