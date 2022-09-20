a039d8985583744c180c5cbd9691e6ddabd8abbf
Harald Pretl <harald@pretl.eu>
2022-09-20 12:33:13 +0200

    Enable PSS in ngspice

--- images/ngspice/scripts/install.sh
+++ images/ngspice/scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 REPO_COMMIT_SHORT=$(echo $NGSPICE_REPO_COMMIT | cut -c 1-7)
 
 git clone ${NGSPICE_REPO_URL} ${NGSPICE_NAME}
@@ -10,6 +9,6 @@ git checkout ${NGSPICE_REPO_COMMIT}
 #FIXME 2nd run of autogen needed
 set -e
 ./autogen.sh
-./configure --disable-debug --enable-openmp --with-x --with-readline=no --enable-xspice --with-fftw3=yes --prefix=/foss/tools/${NGSPICE_NAME}/${REPO_COMMIT_SHORT}
+./configure --disable-debug --enable-openmp --with-x --with-readline=no --enable-pss --enable-xspice --with-fftw3=yes --prefix=/foss/tools/${NGSPICE_NAME}/${REPO_COMMIT_SHORT}
 make -j$(nproc)
 make install
