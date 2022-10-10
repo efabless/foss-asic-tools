3d31664049d8092b4ec22831377484b9d92d769a
Harald Pretl <harald@pretl.eu>
2022-10-10 09:01:10 +0200

    Enable sky130B in addition to sky130A (default still sky130A)

--- images/open_pdks/scripts/install.sh
+++ images/open_pdks/scripts/install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-set -e
+# `-e` should not be set as something exits with an error code, but the run is still OK
+#set -e
 
-magic_version=$(ls /foss/tools/magic/)
-export PATH=$PATH:/foss/tools/magic/$magic_version/bin
+MAGIC_VERSION=$(ls /foss/tools/magic/)
+export PATH=$PATH:/foss/tools/magic/$MAGIC_VERSION/bin
 
 export PDK_ROOT=/foss/pdk
 # select version 'A', or 'B', or 'all'
-export SKY130_VERSION=A
+SKY130_VERSION=all
 
-cd $PDK_ROOT
-git clone ${OPEN_PDKS_REPO_URL} ${OPEN_PDKS_NAME}
-cd ${OPEN_PDKS_NAME}
-git checkout -qf ${OPEN_PDKS_REPO_COMMIT}
+cd $PDK_ROOT || exit
+git clone "${OPEN_PDKS_REPO_URL}" "${OPEN_PDKS_NAME}"
+cd "${OPEN_PDKS_NAME}" || exit
+git checkout -qf "${OPEN_PDKS_REPO_COMMIT}"
 
 ./configure \
 	--enable-sky130-pdk=$PDK_ROOT/skywater-pdk \
@@ -22,17 +23,22 @@ git checkout -qf ${OPEN_PDKS_REPO_COMMIT}
 	--with-sky130-variants=$SKY130_VERSION \
 	--datadir=/foss/
 
-make -j$(nproc)
+make -j"$(nproc)"
 make install
 
-cp $PDK_ROOT/$OPEN_PDKS_NAME/sky130/sky130${SKY130_VERSION}_make.log $PDK_ROOT/sky130${SKY130_VERSION}
+if [ "$SKY130_VERSION" = "all" ]; then
+	cp $PDK_ROOT/"$OPEN_PDKS_NAME"/sky130/sky130A_make.log $PDK_ROOT/sky130A
+	cp $PDK_ROOT/"$OPEN_PDKS_NAME"/sky130/sky130B_make.log $PDK_ROOT/sky130B
+else
+	cp $PDK_ROOT/"$OPEN_PDKS_NAME"/sky130/sky130${SKY130_VERSION}_make.log $PDK_ROOT/sky130${SKY130_VERSION}
+fi
 
-echo "$OPEN_PDKS_NAME $OPEN_PDKS_REPO_COMMIT" 	> "$PDK_ROOT/sky130${SKY130_VERSION}/SOURCES"
-cat "$PDK_ROOT/skywater-pdk/SOURCES" 		>> "$PDK_ROOT/sky130${SKY130_VERSION}/SOURCES"
-cat "/foss/tools/magic/$magic_version/SOURCES" 	>> "$PDK_ROOT/sky130${SKY130_VERSION}/SOURCES"
+echo "$OPEN_PDKS_NAME $OPEN_PDKS_REPO_COMMIT"	> "$PDK_ROOT/sky130${SKY130_VERSION}/SOURCES"
+cat "$PDK_ROOT/skywater-pdk/SOURCES" 			>> "$PDK_ROOT/sky130${SKY130_VERSION}/SOURCES"
+cat "/foss/tools/magic/$MAGIC_VERSION/SOURCES" 	>> "$PDK_ROOT/sky130${SKY130_VERSION}/SOURCES"
 
 make veryclean
 
-cd $PDK_ROOT 
+cd $PDK_ROOT || exit
 rm -rf skywater-pdk open_pdks 
 chmod -R 755 $PDK_ROOT
