5c7571df38a8309f95f001630f3dc0a434e602d2
Harald Pretl <harald@pretl.eu>
2022-10-30 22:36:48 +0100

    Remove FIXME

--- images/gtkwave/scripts/install.sh
+++ images/gtkwave/scripts/install.sh
@@ -8,7 +8,6 @@ git clone "${GTKWAVE_REPO_URL}" "${GTKWAVE_NAME}"
 cd "${GTKWAVE_NAME}/gtkwave3-gtk3/"
 git checkout "${GTKWAVE_REPO_COMMIT}"
 ./autogen.sh
-#FIXME we need to disable TCL due to compile error on aarch64
-./configure --prefix="/foss/tools/${GTKWAVE_NAME}/${REPO_COMMIT_SHORT}" --enable-gtk3 --disable-tcl
+./configure --prefix="/foss/tools/${GTKWAVE_NAME}/${REPO_COMMIT_SHORT}" --enable-gtk3
 make -j"$(nproc)"
 make install
