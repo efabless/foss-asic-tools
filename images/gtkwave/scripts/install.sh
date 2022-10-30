bcda150965aed89d424bfcffc846c216cc2053a3
Harald Pretl <harald@pretl.eu>
2022-10-30 22:43:01 +0100

    Revert "Remove FIXME"
    This reverts commit 5c7571df38a8309f95f001630f3dc0a434e602d2.

--- images/gtkwave/scripts/install.sh
+++ images/gtkwave/scripts/install.sh
@@ -8,6 +8,7 @@ git clone "${GTKWAVE_REPO_URL}" "${GTKWAVE_NAME}"
 cd "${GTKWAVE_NAME}/gtkwave3-gtk3/"
 git checkout "${GTKWAVE_REPO_COMMIT}"
 ./autogen.sh
-./configure --prefix="/foss/tools/${GTKWAVE_NAME}/${REPO_COMMIT_SHORT}" --enable-gtk3
+#FIXME we need to disable TCL due to compile error on aarch64
+./configure --prefix="/foss/tools/${GTKWAVE_NAME}/${REPO_COMMIT_SHORT}" --enable-gtk3 --disable-tcl
 make -j"$(nproc)"
 make install
