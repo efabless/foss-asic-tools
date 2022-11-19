9fa101c574b5dfa357118bc2c58f90fe8d4c5837
Harald Pretl <harald@pretl.eu>
2022-11-19 14:25:39 +0100

    Fix ShellCheck warnings

--- images/fault/scripts/install.sh
+++ images/fault/scripts/install.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-REPO_COMMIT_SHORT=$(echo $FAULT_REPO_COMMIT | cut -c 1-7)
+REPO_COMMIT_SHORT=$(echo "$FAULT_REPO_COMMIT" | cut -c 1-7)
 
-mkdir -p /foss/tools/${FAULT_NAME}/${REPO_COMMIT_SHORT}
-git clone ${FAULT_REPO_URL} ${FAULT_NAME}
-cd ${FAULT_NAME}
-git checkout ${FAULT_REPO_COMMIT}
+mkdir -p "/foss/tools/${FAULT_NAME}/${REPO_COMMIT_SHORT}"
+git clone "${FAULT_REPO_URL}" "${FAULT_NAME}"
+cd "${FAULT_NAME}"
+git checkout "${FAULT_REPO_COMMIT}"
 git submodule update --init --recursive
 
 INSTALL_DIR=/foss/tools/${FAULT_NAME}/${REPO_COMMIT_SHORT} swift ./install.swift
--- images/yosys/scripts/install.sh
+++ images/yosys/scripts/install.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-REPO_COMMIT_SHORT=$(echo $YOSYS_REPO_COMMIT | cut -c 1-7)
+REPO_COMMIT_SHORT=$(echo "$YOSYS_REPO_COMMIT" | cut -c 1-7)
 
-git clone ${YOSYS_REPO_URL} ${YOSYS_NAME}
-cd ${YOSYS_NAME}
-git checkout ${YOSYS_REPO_COMMIT}
-make PREFIX=/foss/tools/${YOSYS_NAME}/${REPO_COMMIT_SHORT} config-gcc
-make PREFIX=/foss/tools/${YOSYS_NAME}/${REPO_COMMIT_SHORT} -j$(nproc)
-make PREFIX=/foss/tools/${YOSYS_NAME}/${REPO_COMMIT_SHORT} install
+git clone "${YOSYS_REPO_URL}" "${YOSYS_NAME}"
+cd "${YOSYS_NAME}"
+git checkout "${YOSYS_REPO_COMMIT}"
+make PREFIX="/foss/tools/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" config-gcc
+make PREFIX="/foss/tools/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" -j"$(nproc)"
+make PREFIX="/foss/tools/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" install
