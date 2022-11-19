a4e800f4bc9e82e4de0ad80cd0322f59d5ef4ae9
Harald Pretl <harald@pretl.eu>
2022-11-19 14:25:52 +0100

    Enable readline

--- images/libngspice/scripts/install.sh
+++ images/libngspice/scripts/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-REPO_COMMIT_SHORT=$(echo $NGSPICE_REPO_COMMIT | cut -c 1-7)
+REPO_COMMIT_SHORT=$(echo "$NGSPICE_REPO_COMMIT" | cut -c 1-7)
 
-git clone ${NGSPICE_REPO_URL} ${NGSPICE_NAME}
-cd ${NGSPICE_NAME}
-git checkout ${NGSPICE_REPO_COMMIT}
+git clone "${NGSPICE_REPO_URL}" "${NGSPICE_NAME}"
+cd "${NGSPICE_NAME}"
+git checkout "${NGSPICE_REPO_COMMIT}"
 ./autogen.sh
 #FIXME 2nd run of autogen needed
 set -e
 ./autogen.sh
-./configure --disable-debug --enable-openmp --with-ngshared --with-x --with-readline=no --enable-pss --enable-xspice --with-fftw3=yes --prefix=/foss/tools/${NGSPICE_NAME}/${REPO_COMMIT_SHORT}
-make -j$(nproc)
+./configure --disable-debug --enable-openmp --with-ngshared --with-x --with-readline=yes --enable-pss --enable-xspice --with-fftw3=yes --prefix=/foss/tools/"${NGSPICE_NAME}"/"${REPO_COMMIT_SHORT}"
+make -j"$(nproc)"
 make install
