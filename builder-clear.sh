6f68a9832f4807dd1ddc69d772a70e2f4ff931ae
Georg Zachl <it@zachlge.org>
2022-11-23 17:06:38 +0100

    Buildscript fix is working, therefore also fixing cleanup script.

--- builder-clear.sh
+++ builder-clear.sh
@@ -15,7 +15,7 @@ if [ -z ${BUILDER_NAME+z} ]; then
 fi
 
 if [ -z ${DOCKER_PLATFORMS+z} ]; then
-        DOCKER_PLATFORMS="amd64,arm64"
+        DOCKER_PLATFORMS="linux/amd64,linux/arm64/v8"
 fi
 
 # delete the builders in the reversed creation order
@@ -26,8 +26,8 @@ fi
 P_PLATS=""
 IFS=',' read -ra P_PLATS <<< "$DOCKER_PLATFORMS"
 for i in "${!P_PLATS[@]}"; do
-	if docker context inspect ${BUILDER_NAME}-${P_PLATS[i]} > /dev/null 2>&1 ; then
-		echo "Deleting docker context ${BUILDER_NAME}-${P_PLATS[i]}"
-		${ECHO_IF_DRY_RUN} docker context rm ${BUILDER_NAME}-${P_PLATS[i]}
+	if docker context inspect ${BUILDER_NAME}-${P_PLATS[i]//\//-} > /dev/null 2>&1 ; then
+		echo "Deleting docker context ${BUILDER_NAME}-${P_PLATS[i]//\//-}"
+		${ECHO_IF_DRY_RUN} docker context rm ${BUILDER_NAME}-${P_PLATS[i]//\//-}
 	fi
 done
