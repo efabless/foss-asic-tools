6e298ef725c2c59454ba4da682917459c23734ff
Harald Pretl <harald@pretl.eu>
2023-01-18 13:34:30 +0100

    Apply shellcheck hints, add INFO

--- images/iic-osic-tools/scripts/set_user_permission.sh
+++ images/iic-osic-tools/scripts/set_user_permission.sh
@@ -7,8 +7,8 @@ fi
 
 for var in "$@"
 do
-    echo "fix permissions for: $var"
-    find "$var"/ -name '*.sh' -exec chmod $verbose a+x {} +
-    find "$var"/ -name '*.desktop' -exec chmod $verbose a+x {} +
-    chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
+    echo "[INFO] Fix permissions for: $var"
+    find "$var"/ -name '*.sh' -exec chmod "$verbose" a+x {} +
+    find "$var"/ -name '*.desktop' -exec chmod "$verbose" a+x {} +
+    chgrp -R 0 "$var" && chmod -R "$verbose" a+rw "$var" && find "$var" -type d -exec chmod "$verbose" a+x {} +
 done
