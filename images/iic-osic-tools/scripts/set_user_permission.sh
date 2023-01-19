2bdcc87e5ef63d13bdeea7b43c1211116268dc14
Harald Pretl <harald@pretl.eu>
2023-01-19 09:28:51 +0100

    Fix script, quotes do not work here

--- images/iic-osic-tools/scripts/set_user_permission.sh
+++ images/iic-osic-tools/scripts/set_user_permission.sh
@@ -8,7 +8,10 @@ fi
 for var in "$@"
 do
     echo "[INFO] Fix permissions for: $var"
-    find "$var"/ -name '*.sh' -exec chmod "$verbose" a+x {} +
-    find "$var"/ -name '*.desktop' -exec chmod "$verbose" a+x {} +
-    chgrp -R 0 "$var" && chmod -R "$verbose" a+rw "$var" && find "$var" -type d -exec chmod "$verbose" a+x {} +
+    # shellcheck disable=SC2086
+    find "$var"/ -name '*.sh' -exec chmod $verbose a+x {} +
+    # shellcheck disable=SC2086
+    find "$var"/ -name '*.desktop' -exec chmod $verbose a+x {} +
+    # shellcheck disable=SC2086
+    chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
 done
