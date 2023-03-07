474bdf27983aee666f3b046fef3947f4824ff6a9
JakobRat <jakob.rat@me.com>
2023-03-07 10:23:30 +0100

    Update install.sh

--- images/align/scripts/install.sh
+++ images/align/scripts/install.sh
@@ -4,6 +4,8 @@ set -e
 
 REPO_COMMIT_SHORT=$(echo "$ALIGN_REPO_COMMIT" | cut -c 1-7)
 
+mkdir -p /foss/tools
+cd /foss/tools
 git clone "${ALIGN_REPO_URL}" "${ALIGN_NAME}"
 cd "${ALIGN_NAME}"
 git checkout "${ALIGALIGN_REPO_COMMIT}"
