4e9ccaabb1d0eb6a04a28e8672de10b5037b8449
JakobRat <jakob.rat@me.com>
2023-03-07 10:09:06 +0100

    Update install.sh

--- images/align-pdk-sky130/scripts/install.sh
+++ images/align-pdk-sky130/scripts/install.sh
@@ -4,6 +4,8 @@ set -e
 
 REPO_COMMIT_SHORT=$(echo "$ALIGN_PDK_SKY130_REPO_COMMIT" | cut -c 1-7)
 
+mkdir -p /foss/tools
+cd /foss/tools
 git clone "${ALIGN_PDK_SKY130_REPO_URL}" "${ALIGN_PDK_SKY130_NAME}"
 cd "${ALIGN_PDK_SKY130_NAME}"
 git checkout "${ALIGN_PDK_SKY130_REPO_COMMIT}"
