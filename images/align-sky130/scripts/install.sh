b4cba1e23a7f1c46283eb9bf81c0efcefb88bee6
JakobRat <jakob.rat@me.com>
2023-03-07 07:56:43 +0100

    Updated ALIGN-names.

--- images/align-sky130/scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-REPO_COMMIT_SHORT=$(echo "$ALIGN_SKY130_REPO_COMMIT" | cut -c 1-7)
-
-git clone "${ALIGN_SKY130_REPO_URL}" "${ALIGN_SKY130_NAME}"
-cd "${ALIGN_SKY130_NAME}"
-git checkout "${ALIGN_SKY130_NAME}"
