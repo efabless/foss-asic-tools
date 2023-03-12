52c788b033e19fd3ce0fa7e17157438a0eca4f19
Harald Pretl <harald.pretl@jku.at>
2023-03-12 17:48:00 +0100

    Include IHP sg13g2 PDK

---
+++ images/open_pdks/scripts/install_ihp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "$PDK_ROOT" ]; then
+    mkdir -p "$PDK_ROOT"
+fi
+
+####################
+# INSTALL IHP-SG13G2
+####################
+
+PDK_VERSION="ihp-sg13g2"
+
+cd /tmp || exit
+git clone https://github.com/IHP-GmbH/IHP-Open-PDK.git ihp
+cd ihp || exit
+
+if [ -d $PDK_VERSION ]; then
+	mv $PDK_VERSION "$PDK_ROOT"
+fi
