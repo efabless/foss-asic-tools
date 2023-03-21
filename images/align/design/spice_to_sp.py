f3d4fef58f50a6117e810eba7d9386221f333e6e
JakobRat <jakob.rat@me.com>
2023-03-21 14:05:38 +0100

    Added apache licence.

--- images/align/design/spice_to_sp.py
+++ images/align/design/spice_to_sp.py
@@ -1,17 +1,30 @@
 # -*- coding: utf-8 -*-
-"""
-(c) 2023 Jakob Ratschenberger
-Johannes Kepler University Linz, Institute for Integrated Circuits
+# ========================================================================
+# SPDX-FileCopyrightText: 2023 Jakob Ratschenberger
+# Johannes Kepler University, Institute for Integrated Circuits
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+#
+# This script generates the ALIGN .sp format, from a spice netlist.
+#
+# The following devices are supported:
+#    -sky130_fd_pr__nfet_01v8
+#    -sky130_fd_pr__pfet_01v8
+#
+# A subcircuit component must start with the component identifier "x".
+# ========================================================================
 
-Script to change a spice netlist to the ALIGN .sp format.
-
-The following devices are supported:
-    -sky130_fd_pr__nfet_01v8
-    -sky130_fd_pr__pfet_01v8
-
-A subcircuit component must start with the component identifier "x".
-
-"""
 import sys 
 import argparse
 
