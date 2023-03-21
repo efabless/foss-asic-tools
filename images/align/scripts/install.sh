13f4846695ab2cbb0d63114b3bb49300ccec9308
Harald Pretl <harald.pretl@jku.at>
2023-03-21 13:31:44 +0100

    Update build scripts

--- images/align/scripts/install.sh
+++ images/align/scripts/install.sh
@@ -5,15 +5,16 @@ set -e
 REPO_COMMIT_SHORT=$(echo "$ALIGN_REPO_COMMIT" | cut -c 1-7)
 
 mkdir -p /foss/tools
-cd /foss/tools
-git clone "${ALIGN_REPO_URL}" "${ALIGN_NAME}"
-cd "${ALIGN_NAME}"
-git checkout "${ALIGN_REPO_COMMIT}"
+#cd /foss/tools
+git clone "$ALIGN_REPO_URL" "/foss/tools/$ALIGN_NAME/$REPO_COMMIT_SHORT"
+cd "/foss/tools/$ALIGN_NAME/$REPO_COMMIT_SHORT" || exit 1
+git checkout "$ALIGN_REPO_COMMIT"
 
 #create a python virtualenv
-python3 -m venv general --without-pip --system-site-packages
-source general/bin/activate
-python3 -m pip install pip --upgrade
+#python3 -m venv general --without-pip --system-site-packages
+#source general/bin/activate
+#python3 -m pip install pip --upgrade
+pip3 install . --prefix="/foss/tools/$ALIGN_NAME/$REPO_COMMIT_SHORT" --no-cache-dir
 
 #install align as a user
-pip install -v .
+#pip install -v .
--- images/iic-osic-tools/scripts/env.sh
+++ images/iic-osic-tools/scripts/env.sh
@@ -73,6 +73,7 @@ if [ -z ${FOSS_PATH_SET+x} ]; then
         # FIXME: OpenROAD in Ubuntu 22.04 does not find the PIP modules, so use PYTHONPATH
         PYTHONPATH=$(python -c "import sys; print(':'.join(x for x in sys.path if x))") && export PYTHONPATH 
         _path_add_tool_python "ngspyce"
+        _path_add_tool_python "align"
 
         echo "[INFO] Final PATH variable: $PATH"
         echo "[INFO] Final PYTHONPATH variable: $PYTHONPATH"
