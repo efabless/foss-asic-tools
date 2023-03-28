e5b3af1e3700586342020be128f9c248bcb4625c
JakobRat <jakob.rat@me.com>
2023-03-28 10:49:27 +0200

    Update env.sh

--- images/iic-osic-tools/scripts/env.sh
+++ images/iic-osic-tools/scripts/env.sh
@@ -121,8 +121,6 @@ alias xschemtcl='xschem --rcfile $PDKPATH/libs.tech/xschem/xschemrc'
 
 alias openlane='openlane --manual-pdk'
 
-# alias iicalign='bash $TOOLS/align/design/iic-align.sh'
-
 alias tt='cd $TOOLS'
 alias dd='cd $DESIGNS'
 alias pp='cd $PDK_ROOT'
