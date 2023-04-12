c6f857facca71d2ea562b7eb44f24ed4be0c9fd6
Harald Pretl <harald.pretl@jku.at>
2023-04-12 18:47:30 +0200

    Temporay WA for OpenSTA segfault

--- images/openroad/scripts/install.sh
+++ images/openroad/scripts/install.sh
@@ -9,6 +9,10 @@ cd "${OPENROAD_APP_NAME}"
 git checkout "${OPENROAD_APP_REPO_COMMIT}"
 git submodule update --init --recursive
 
+# FIXME WA for OpenSTA segfault (will be fixed in OpenROAD later), see https://github.com/The-OpenROAD-Project/OpenLane/issues/1739
+cd src/sta && curl -L https://github.com/jjcherry56/OpenSTA/commit/974d9f43e7ba15a56f8534b36d7a107ef690602d.diff | patch --strip=1 
+cd ../..
+
 mkdir -p build && cd build
 cmake .. "-DCMAKE_INSTALL_PREFIX=/foss/tools/${OPENROAD_APP_NAME}/${REPO_COMMIT_SHORT}" "-DUSE_SYSTEM_BOOST=ON"
 make -j"$(nproc)"
