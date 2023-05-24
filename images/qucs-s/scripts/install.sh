f2a31cadd722ed57142bb16e7fe39fadbca7ac0f
Harald Pretl <harald.pretl@jku.at>
2023-05-24 14:41:49 +0200

    Script cleanup

--- images/qucs-s/scripts/install.sh
+++ images/qucs-s/scripts/install.sh
@@ -4,10 +4,10 @@ set -e
 
 REPO_COMMIT_SHORT=$(echo "$QUCS_REPO_COMMIT" | cut -c 1-7)
 
-git clone "${QUCS_REPO_URL}" "${QUCS_NAME}"
-cd "${QUCS_NAME}"
-git checkout "${QUCS_REPO_COMMIT}"
+git clone "$QUCS_REPO_URL" "$QUCS_NAME"
+cd "$QUCS_NAME"
+git checkout "$QUCS_REPO_COMMIT"
 mkdir build && cd build
-cmake ..  -DCMAKE_INSTALL_PREFIX="${TOOLS}/${QUCS_NAME}/${REPO_COMMIT_SHORT}"
+cmake ..  -DCMAKE_INSTALL_PREFIX="$TOOLS/$QUCS_NAME/$REPO_COMMIT_SHORT"
 make -j"$(nproc)"
 make install
