d09a9ef23d3b09b0105a34a2ab2b3aeb039152b6
Harald Pretl <harald.pretl@jku.at>
2023-07-07 12:20:46 +0200

    Only builds on `amd64` for now

--- images/align-pdk-sky130/scripts/install.sh
+++ images/align-pdk-sky130/scripts/install.sh
@@ -4,7 +4,13 @@ set -e
 
 REPO_COMMIT_SHORT=$(echo "$ALIGN_PDK_SKY130_REPO_COMMIT" | cut -c 1-7)
 
-mkdir -p "$TOOLS"
-git clone "$ALIGN_PDK_SKY130_REPO_URL" "${TOOLS}/$ALIGN_PDK_SKY130_NAME"
-cd "${TOOLS}/$ALIGN_PDK_SKY130_NAME"
-git checkout "$ALIGN_PDK_SKY130_REPO_COMMIT"
+if [ "$(arch)" == "x86_64" ]; then
+    #FIXME compile fail on arm64 needs fixing
+    mkdir -p "$TOOLS"
+    git clone --filter=blob:none "$ALIGN_PDK_SKY130_REPO_URL" "${TOOLS}/$ALIGN_PDK_SKY130_NAME"
+    cd "${TOOLS}/$ALIGN_PDK_SKY130_NAME"
+    git checkout "$ALIGN_PDK_SKY130_REPO_COMMIT"
+else
+    mkdir -p "${TOOLS}/$ALIGN_PDK_SKY130_NAME"
+    echo "This CPU architecture is not supported yet!" > "${TOOLS}/$ALIGN_PDK_SKY130_NAME/README.txt"
+fi
--- images/align/scripts/install.sh
+++ images/align/scripts/install.sh
@@ -4,12 +4,16 @@ set -e
 
 REPO_COMMIT_SHORT=$(echo "$ALIGN_REPO_COMMIT" | cut -c 1-7)
 
-mkdir -p "$TOOLS"
-#cd /foss/tools
-git clone "$ALIGN_REPO_URL" "${TOOLS}/$ALIGN_NAME/$REPO_COMMIT_SHORT"
-cd "${TOOLS}/$ALIGN_NAME/$REPO_COMMIT_SHORT" || exit 1
-git checkout "$ALIGN_REPO_COMMIT"
+if [ "$(arch)" == "x86_64" ]; then
+    #FIXME compile fail on arm64 needs fixing
+    mkdir -p "$TOOLS"
+    git clone --filter=blob:none "$ALIGN_REPO_URL" "${TOOLS}/$ALIGN_NAME/$REPO_COMMIT_SHORT"
+    cd "${TOOLS}/$ALIGN_NAME/$REPO_COMMIT_SHORT" || exit 1
+    git checkout "$ALIGN_REPO_COMMIT"
 
-#python3 setup.py install
-pip3 install . --prefix="${TOOLS}/$ALIGN_NAME/$REPO_COMMIT_SHORT" --no-cache-dir
-
+    #python3 setup.py install
+    pip3 install . --prefix="${TOOLS}/$ALIGN_NAME/$REPO_COMMIT_SHORT" --no-cache-dir
+else
+    mkdir -p "${TOOLS}/$ALIGN_NAME"
+    echo "This CPU architecture is not supported yet!" > "${TOOLS}/$ALIGN_NAME/README.txt"
+fi
