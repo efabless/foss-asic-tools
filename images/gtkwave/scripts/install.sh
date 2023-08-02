75c395d77c3a8a0928226443e58fea70734ae069
Harald Pretl <harald.pretl@jku.at>
2023-08-02 08:23:37 +0200

    Fix build fail

--- images/gtkwave/scripts/install.sh
+++ images/gtkwave/scripts/install.sh
@@ -5,7 +5,7 @@ set -e
 REPO_COMMIT_SHORT=$(echo "$GTKWAVE_REPO_COMMIT" | cut -c 1-7)
 
 git clone --filter=blob:none "${GTKWAVE_REPO_URL}" "${GTKWAVE_NAME}"
-cd "${GTKWAVE_NAME}/gtkwave3-gtk3/"
+cd "${GTKWAVE_NAME}"
 git checkout "${GTKWAVE_REPO_COMMIT}"
 ./autogen.sh
 ./configure --prefix="${TOOLS}/${GTKWAVE_NAME}/${REPO_COMMIT_SHORT}" --enable-gtk3
