120595fc50494edf382e8c790ca451710014c0d1
Harald Pretl <harald.pretl@jku.at>
2023-10-14 13:44:28 +0200

    Enable OSDI for IHP PDK

--- _build/images/ngspice/scripts/install.sh
+++ _build/images/ngspice/scripts/install.sh
@@ -11,7 +11,7 @@ set -e
 ./autogen.sh
 
 # Define common compile options
-NGSPICE_COMPILE_OPTS=("--disable-debug" "--enable-openmp" "--with-x" "--with-readline=yes" "--enable-pss" "--enable-xspice" "--with-fftw3=yes")
+NGSPICE_COMPILE_OPTS=("--disable-debug" "--enable-openmp" "--with-x" "--with-readline=yes" "--enable-pss" "--enable-xspice" "--with-fftw3=yes" "--enable-osdi")
 
 # Compile ngspice executable
 ./configure "${NGSPICE_COMPILE_OPTS[@]}" --prefix="${TOOLS}/${NGSPICE_NAME}/${REPO_COMMIT_SHORT}"
