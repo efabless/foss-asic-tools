4cc43cf0b9ed446cbd73b27c08c82c3cd600dd23
Harald Pretl <harald.pretl@jku.at>
2023-10-15 20:23:24 +0200

    Add comment that LLVM<15 is needed

--- _build/images/ghdl/scripts/install.sh
+++ _build/images/ghdl/scripts/install.sh
@@ -7,6 +7,8 @@ REPO_COMMIT_SHORT=$(echo "$GHDL_REPO_COMMIT" | cut -c 1-7)
 git clone --filter=blob:none "${GHDL_REPO_URL}" "${GHDL_NAME}"
 cd "${GHDL_NAME}"
 git checkout "${GHDL_REPO_COMMIT}"
+
+# LLVM compile (needs LLVM<15)
 ./configure  --with-llvm-config --prefix="${TOOLS}/${GHDL_NAME}/${REPO_COMMIT_SHORT}"
 make -j"$(nproc)"
 make install
