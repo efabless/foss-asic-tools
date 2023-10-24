50847cc5ace211cc0ce54763e377c166f7dcebc8
Harald Pretl <harald.pretl@jku.at>
2023-10-24 10:22:35 +0200

    Switch back to IHP repo

--- _build/images/open_pdks/scripts/install_ihp.sh
+++ _build/images/open_pdks/scripts/install_ihp.sh
@@ -14,7 +14,8 @@ PDK_VERSION="ihp-sg13g2"
 
 cd /tmp || exit
 #git clone --depth=1 https://github.com/IHP-GmbH/IHP-Open-PDK.git ihp
-git clone https://github.com/iic-jku/IHP-Open-PDK.git ihp
+git clone https://github.com/IHP-GmbH/IHP-Open-PDK.git ihp
+#git clone https://github.com/iic-jku/IHP-Open-PDK.git ihp
 cd ihp || exit
 #FIXME for now uses branch "dev" to get the latest releases
 git checkout dev
@@ -28,6 +29,7 @@ fi
 cd "$PDK_ROOT"/"$PDK_VERSION"/libs.tech/ngspice/openvaf
 
 # Get OpenVAF
+#FIXME compile as part of Docker build, instead of pulling executable
 OPENFAV_FILE=openvaf.$(arch)
 OPENFAV_URL=https://github.com/iic-jku/osic-multitool/raw/main/openvaf/$OPENFAV_FILE.gz
 wget "$OPENFAV_URL" && gunzip "$OPENFAV_FILE.gz" && mv "$OPENFAV_FILE" openvaf && chmod +x openvaf
