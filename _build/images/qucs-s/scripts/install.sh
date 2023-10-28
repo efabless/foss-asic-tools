f6e83dbb24839e58a703c39b214add048bb07d53
Harald Pretl <harald.pretl@jku.at>
2023-10-28 15:51:36 +0200

    Bump tool versions, rename `QUCS` variables to `QUCS_S`

--- _build/images/qucs-s/scripts/install.sh
+++ _build/images/qucs-s/scripts/install.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-REPO_COMMIT_SHORT=$(echo "$QUCS_REPO_COMMIT" | cut -c 1-7)
+REPO_COMMIT_SHORT=$(echo "$QUCS_S_REPO_COMMIT" | cut -c 1-7)
 
-git clone --filter=blob:none "$QUCS_REPO_URL" "$QUCS_NAME"
-cd "$QUCS_NAME"
-git checkout "$QUCS_REPO_COMMIT"
+git clone --filter=blob:none "$QUCS_S_REPO_URL" "$QUCS_S_NAME"
+cd "$QUCS_S_NAME"
+git checkout "$QUCS_S_REPO_COMMIT"
 mkdir build && cd build
-cmake ..  -DCMAKE_INSTALL_PREFIX="$TOOLS/$QUCS_NAME/$REPO_COMMIT_SHORT"
+cmake ..  -DCMAKE_INSTALL_PREFIX="$TOOLS/$QUCS_S_NAME/$REPO_COMMIT_SHORT"
 make -j"$(nproc)"
 make install
