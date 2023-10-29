ec8df88983f3671b981c7a12bbf0ed14d3477b36
Harald Pretl <harald.pretl@jku.at>
2023-10-29 17:28:49 +0100

    Add yosys eqy, sby, mcy

--- _build/images/yosys/scripts/install.sh
+++ _build/images/yosys/scripts/install.sh
@@ -10,3 +10,26 @@ git checkout "${YOSYS_REPO_COMMIT}"
 make PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" config-gcc
 make PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" -j"$(nproc)"
 make PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" install
+cd ..
+
+# Install yosys.eqy
+git clone --filter=blob:none https://github.com/YosysHQ/eqy.git
+cd eqy
+sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}#g" Makefile
+make -j$(nproc)
+make install
+cd ..
+
+# Install yosys.sby
+git clone --filter=blob:none https://github.com/YosysHQ/sby.git
+cd sby
+sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}#g" Makefile
+make install
+cd ..
+
+# Install yosys.mcy
+git clone --filter=blob:none https://github.com/YosysHQ/mcy.git
+cd mcy
+sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}#g" Makefile
+make install
+cd ..
