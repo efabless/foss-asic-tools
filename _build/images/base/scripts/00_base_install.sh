07ba3096e44ea8da3c235ba731fba521c778da56
Harald Pretl <harald.pretl@jku.at>
2024-03-10 08:57:56 +0100

    Get many Python packages from APT (rather then PIP)

--- _build/images/base/scripts/00_base_install.sh
+++ _build/images/base/scripts/00_base_install.sh
@@ -135,28 +135,134 @@ apt-get -y install \
 	pciutils \
 	pkg-config \
 	python3 \
+	python3-aiohttp \
+	python3-aiosignal \
+	python3-aiosqlite \
+	python3-altair \
+	python3-antlr4 \
+	python3-anyio \
+	python3-argon2 \
+	python3-arrow \
+	python3-asttokens \
+	python3-babel \
+	python3-bleach \
+	python3-cachetools \
+	python3-charset-normalizer \
+	python3-cmd2 \
+	python3-cffi \
 	python3-click \
+	python3-commonmark \
+	python3-coverage \
 	python3-cvxopt \
+	python3-defusedxml \
+	python3-deprecated \
 	python3-dev \
+	python3-distro \
+	python3-executing \
+	python3-fastjsonschema \
 	python3-flask \
+	python3-frozenlist \
+	python3-fsspec \
+	python3-gdspy \
+	python3-gitdb \
 	python3-graphviz \
+	python3-greenlet \
+	python3-h11 \
+	python3-httpcore \
+	python3-httpx \
+	python3-immutabledict \
+	python3-ipykernel \
+	python3-ipython \
+	python3-ipywidgets \
+	python3-iso8601 \
+	python3-isodate \
+	python3-jedi \
+	python3-joblib \
+	python3-json5 \
+	python3-jsonschema \
+	python3-jupyter-core \
+	python3-jupyter-client \
+	python3-jupyter-console \
+	python3-jupyter-server \
+	python3-jupyterlab-server \
+	python3-linecache2 \
+	python3-loguru \
+	python3-lxml \
 	python3-matplotlib \
+	python3-matplotlib-inline \
+	python3-mistune \
+	python3-mpi4py \
+	python3-nbclient \
+	python3-nbconvert \
+	python3-nbformat \
+	python3-nest-asyncio \
+	python3-netifaces \
+	python3-networkx \
+	python3-notebook \
 	python3-numpy \
 	python3-orderedmultidict \
+	python3-pandas \
+	python3-pandocfilters \
+	python3-parso \
+	python3-pexpect \
+	python3-prettytable \
+	python3-prometheus-client \
+	python3-prompt-toolkit \
+	python3-protobuf \
+	python3-ptyprocess \
+	python3-pure-eval \
 	python3-pip \
 	python3-plotly \
 	python3-psutil \
 	python3-pybind11 \
+	python3-pycparser \
+	python3-pydantic \
 	python3-pygame \
+	python3-pyperclip \
+	python3-pyqtgraph \
 	python3-pyqt5 \
 	python3-pytest \
+	python3-qrcode \
+	python3-qtconsole \
+	python3-qtpy \
+	python3-rdflib \
+	python3-requests \
+	python3-rich \
 	python3-rtree \
 	python3-scipy \
+	python3-send2trash \
 	python3-setuptools \
 	python3-setuptools-rust \
 	python3-shapely \
+	python3-smmap \
+	python3-sniffio \
+	python3-stack-data \
+	python3-streamlink \
+	python3-tabulate \
+	python3-tenacity \
+	python3-terminado \
+	python3-testtools \
+	python3-threadpoolctl \
+	python3-tinycss2 \
+	python3-tomli \
+	python3-toolz \
+	python3-torch \
+	python3-tornado \
+	python3-traitlets \
+	python3-typer \
+	python3-typing-extensions \
+	python3-tzlocal \
+	python3-unittest2 \
+	python3-uritemplate \
+	python3-validators \
+	python3-voluptuous \
 	python3-tk \
 	python3-tqdm \
+	python3-watchdog \
+	python3-wcwidth \
+	python3-webcolors \
+	python3-widgetsnbextension \
+	python3-wrapt \
 	python3-venv \
 	python3-virtualenv \
 	python3-wheel \
